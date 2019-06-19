3c0722920bdcc1fd7bca19406810dd048f2a2be5
Steven <stevenanthonyzanga@gmail.com>
2019-06-19 14:41:06 -0400

    added pages

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/Views/index.html");
   });
 
+app.get("/gettingStarted", (req, res) => {
+    res.sendFile(__dirname + "/Views/gettingStarted.html");
+  });
+
+app.get("/browse", (req, res) => {
+    res.sendFile(__dirname + "/Views/browse.html");
+  });
+
+app.get("/paperwork", (req, res) => {
+    res.sendFile(__dirname + "/Views/paperwork.html");
+  });
+
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/Views/contact.html");
+  });  
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started on Port: " + PORT));
